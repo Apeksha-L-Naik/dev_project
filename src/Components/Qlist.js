5ea6ebc840b9aeaef58661634d12e6f3a0553850
Apeksha L Naik <apekshanaik0904@gmail.com>
2024-05-17 11:59:39 +0530

    dev project

--- src/Components/Qlist.js
+++ src/Components/Qlist.js
@@ -1,58 +1,58 @@
-import React,{useEffect, useState} from 'react'
-import Quesion from './Quesion'
+import React, { useState } from 'react';
+import Quesion from './Quesion';
 
 const Qlist = () => {
-  const[ask,setAsk]=useState(false);
+  const [ask, setAsk] = useState(false);
 
-  const handleOnClick=()=>{
+  const handleOnClick = () => {
     setAsk(true);
   };
 
-  const handleOnClose=()=>{
+  const handleOnClose = () => {
     setAsk(false);
-  };  
-  // useEffect(()=>{
-  //   const handleOnClickOutside=(event)=>{
-  //     if(ask&&!event.target.closest('.question-body')){
-  //       setAsk(false);
-  //     }
-  //   };
-  //   document.body.addEventListener('click',handleOnClickOutside);
-  //   return()=>{
-  //     document.body.removeEventListener('click',handleOnClickOutside);
-  //   };
-  // },[ask])
+  };
+
   return (
     <div className='main-body'>
-        <div className='qlist'>
-            <div className='latest-list'>
-                <div className='latest-qlist'>
-                  <div className='latest'>
-                     <h2>LATEST <br/>QUESTION</h2>
-                  </div>
-                  <div className='search'>
-                    <input type='search' placeholder='search'/>
-                  </div>
-                </div>
-                <div></div>
-                <div className='qlist-button'>
-                    <button type='submit' onClick={handleOnClick}>Ask</button>
-                </div>
-                {ask&&(
-                  <>
-                    <Quesion onClose={handleOnClose}/>
-                  </>
-                  )}
+      <div className={`qlist ${ask ? 'blur' : ''}`}>
+        <div className='latest-list'>
+          <div className='latest-qlist'>
+            <div className='latest'>
+              <h2>
+                LATEST <br />
+                QUESTION
+              </h2>
             </div>
-            <div className='top-qlist' >
-                <div className='topqlist-title'>
-                    <h3>Top <br/>Questions</h3>
-                </div>
-                <div className='topqlist-border'></div>
+            <div className='search'>
+              <input type='search' placeholder='search' />
             </div>
+          </div>
+          <div>
+            <frames></frames>
+          </div>
+          <div className='qlist-button'>
+            <button type='submit' onClick={handleOnClick}>
+              Ask
+            </button>
+          </div>
         </div>
+        <div className='top-qlist'>
+          <div className='topqlist-title'>
+            <h3>
+              Top <br />
+              Questions
+            </h3>
+          </div>
+          <div className='topqlist-border'></div>
+        </div>
+      </div>
+      {ask && (
+        <div className='question-overlay'>
+          <Quesion onClose={handleOnClose} />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Qlist
+export default Qlist;
